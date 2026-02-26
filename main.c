0f69891e1ca69f2802131e37554040653e8f36c8
ellie <elliethefemboyy@gmail.com>
2026-02-26 19:04:47 +0800

    removed unnecessary code and make the skeleton of a  rudimentary flag/options system

--- main.c
+++ main.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
 int main(int argc, char **argv) {
     FILE *f;
-    if (argc >= 3) {
+    if (argc >= 2) {
         for (int i = 1; i < argc; i++) {
-            f = fopen(argv[i], "r");
-            if (!f) {
-                perror(argv[i]);
+            if (i == 1 && argv[i][0] == '-') {
+                fprintf(stderr, "Flags not implemented");
                 return 1;
             }
-            int ch;
-            while ((ch = fgetc(f)) != EOF) {
-                putchar(ch);
+            else {
+                f = fopen(argv[i], "r");
+                if (!f) {
+                    perror(argv[i]);
+                    return 1;
+                }
+                int ch;
+                while ((ch = fgetc(f)) != EOF) {
+                    putchar(ch);
+                }
             }
-
         }
         return 0;
     }
-    else if (argc == 1) {
+    else {
         int ch;
         while ((ch = fgetc(stdin)) != EOF) {
             putchar(ch);
         }
         return 1;
-    }
-    else {
-        f = fopen(argv[1], "r");
-        if (!f) {
-            perror(argv[1]);
-            return 1;
-        }
-        int ch;
-        while ((ch = fgetc(f)) != EOF) {
-            putchar(ch);
-        }
     }
 }
