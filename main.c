3bc08a2d51089da4a8c6c60294120783efafe298
ellie <elliethefemboyy@gmail.com>
2026-02-26 19:32:18 +0800

    moved file declaration inside loop to prevent state confusion

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 int main(int argc, char **argv) {
     int ch;
     if (argc >= 2) {
-        FILE *f;
         for (int i = 1; i < argc; i++) {
             if (argv[i] && argv[i][0] == '-') {
                 fprintf(stderr, "Flags not implemented\n");
                 return 1;
             }
             else {
+                FILE *f;
                 f = fopen(argv[i], "r");
                 if (!f) {
                     perror(argv[i]);
