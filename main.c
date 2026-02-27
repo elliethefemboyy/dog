129ab5526e8e8a402083da695192fcc71f7632d6
ellie <elliethefemboyy@gmail.com>
2026-02-27 14:29:46 +0800

    commented out flag checking and removed setvbuf and made max buffer size smaller

--- main.c
+++ main.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 1048576
+#define MAX_SIZE 8192
 int main(const int argc, char **argv) {
     int ch;
     if (argc >= 2) {
-        for (int i = 1; i < argc; i++) {
-            // flag check
-            if (argv[i] && argv[i][0] == '-' || !argv[i+1]) {
-                if (argv[i+1] && argv[i+1][0] == '-') {
-                    fprintf(stderr, "Usage: %s [options] [file] [options] [file]\n", argv[0]);
-                    exit(1);
-                }
-
-            }
-        }
+        // for (int i = 1; i < argc; i++) { // flag checking -- commented out due to not being used
+        //
+        //     if (argv[i] && argv[i][0] == '-' || !argv[i+1]) {
+        //         if (argv[i+1] && argv[i+1][0] == '-') {
+        //             fprintf(stderr, "Usage: %s [options] [file] [options] [file]\n", argv[0]);
+        //             exit(1);
+        //         }
+        //
+        //     }
+        // }
            for (int i = 1; i < argc; i++) { // main concat
-               if (argv[i] && argv[i][0] == '-') {
-                    continue;
-               }
+           //     if (argv[i] && argv[i][0] == '-') {
+           //          continue;
+           //     }
                 FILE *f = fopen(argv[i], "r");
                if (!f) {
                    perror(argv[i]);
                    exit(1);
                }
                char buffer[MAX_SIZE];
-
-               if (setvbuf(f, buffer, _IOFBF, sizeof(buffer)) != 0) {
-                   perror("Error setting buffer mode");
-                   return 1;
-               }
                size_t bytesRead;
 
                while ((bytesRead = fread(buffer, 1, sizeof(buffer), f)) > 0) {
