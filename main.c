f5964c6bde0248c8d3960565015f2ac55a3be781
ellie <elliethefemboyy@gmail.com>
2026-02-28 09:40:29 +0800

    moved file processing into a function and made fallback use buffered logic

--- main.c
+++ main.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_SIZE 8192
+
+void process_file(FILE *f) {
+    char buffer[MAX_SIZE];
+    size_t bytesRead;
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), f)) > 0) {
+        fwrite(buffer, 1, bytesRead, stdout);
+    }
+}
+
 int main(const int argc, char **argv) {
     if (argc >= 2) {
            for (int i = 1; i < argc; i++) { // main concat
                 FILE *f = fopen(argv[i], "r");
                if (!f) {
                    perror(argv[i]);
-                   exit(1);
                }
-               char buffer[MAX_SIZE];
-               size_t bytesRead;
-
-               while ((bytesRead = fread(buffer, 1, sizeof(buffer), f)) > 0) {
-                   fwrite(buffer, 1, bytesRead, stdout);
-               }
-               fflush(stdout);
+               process_file(f);
                 fclose(f);
             }
+        fflush(stdout);
         exit(0);
         }
-    // fallback for no args
-    int ch;
-    while ((ch = fgetc(stdin)) != EOF) {
-            putchar(ch);
-        }
+    process_file(stdin);
         exit(0);
 }
