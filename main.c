646ee95cf309133d608fbf7477111c5f56219f61
ellie <elliethefemboyy@gmail.com>
2026-02-28 10:04:11 +0800

    used malloc for buffer and fixed some possible null file and memory issues

--- main.c
+++ main.c
@@ -3,32 +3,32 @@
 #define MAX_SIZE 8192
 
 void process_file(FILE *f) {
-    char buffer[MAX_SIZE];
+    char *buffer = malloc(MAX_SIZE);
     size_t bytesRead;
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), f)) > 0) {
         fwrite(buffer, 1, bytesRead, stdout);
     }
+    free(buffer);
 }
 
 int main(const int argc, const char *argv[]) {
     if (argc >= 2) {
         int failures = 0;
-           for (int i = 1; i < argc; i++) { // main concat
-                FILE *f = fopen(argv[i], "r");
-               if (!f) {
-                   perror(argv[i]);
-                   failures++;
-                   fclose(f);
-                   continue;
-               }
-               process_file(f);
-                fclose(f);
+        for (int i = 1; i < argc; i++) { // main concat
+            FILE *f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                failures++;
+                continue;
             }
+            process_file(f);
+            fclose(f);
+        }
         if (failures > 0) {
-          exit(0);
+            return 1;
         }
-       exit(1);
-        }
+        return 0;
+    }
     process_file(stdin);
-        exit(0);
+        return 0;
 }
