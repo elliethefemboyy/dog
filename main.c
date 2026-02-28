611f2570caa831df6ce92c048ac487bd7847aad7
ellie <elliethefemboyy@gmail.com>
2026-02-28 09:30:54 +0800

    removed commented out flag checking for conciseness and readability

--- main.c
+++ main.c
@@ -4,20 +4,7 @@
 int main(const int argc, char **argv) {
     int ch;
     if (argc >= 2) {
-        // for (int i = 1; i < argc; i++) { // flag checking -- commented out due to not being used
-        //
-        //     if (argv[i] && argv[i][0] == '-' || !argv[i+1]) {
-        //         if (argv[i+1] && argv[i+1][0] == '-') {
-        //             fprintf(stderr, "Usage: %s [options] [file] [options] [file]\n", argv[0]);
-        //             exit(1);
-        //         }
-        //
-        //     }
-        // }
            for (int i = 1; i < argc; i++) { // main concat
-           //     if (argv[i] && argv[i][0] == '-') {
-           //          continue;
-           //     }
                 FILE *f = fopen(argv[i], "r");
                if (!f) {
                    perror(argv[i]);
